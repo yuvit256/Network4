c25f724b50b456326bd88be4d6fa8319ca6dd030
ron12120 <76705730+ron12120@users.noreply.github.com>
2023-05-03 20:52:21 +0300

    Add files via upload

--- Receiver.py
+++ Receiver.py
@@ -1,24 +1,66 @@
-from socket import *
+import socket
+import io
+import time
 
-serverPort = 8888
-serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.bind(('127.0.0.1',serverPort))
-serverSocket.listen(1)
-print ('The server is ready to receive\n')
-while True:
-    connectionSocket, addr = serverSocket.accept()
-    # start timer
-    with open('received.txt', 'wb') as file:
-        data = connectionSocket.recv(1024)
-        while data:
-            file.write(data)
-            data = connectionSocket.recv(1024)
-    # end timer
-    print ('The first half of the file received\n')
 
-    yuval_id = 8039
-    ron_id = 7214
-    xor = str(yuval_id ^ ron_id)
-    connectionSocket.send(xor.encode())
+SERVER_ADDR = ("127.0.0.1", 8888)
+NUM_CONNECTIONS = 300
+BUFFER_SIZE = 8192
+AUTH = f"{(8039) ^ (7214)}".encode()
+TIMEOUT = 0.001
+OK_MSG = b"Yuval&Ron"
 
-    connectionSocket.close()
+def main():
+    reno = []
+    cubic = []
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP) as server:
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server.bind(SERVER_ADDR)
+        server.listen(NUM_CONNECTIONS)
+        client, addr = server.accept()
+        
+        while True:
+            start1 = time.time()
+            with open("received_part1.txt", "wb") as file1:
+                while True:
+                    data = client.recv(BUFFER_SIZE)
+                    if data.__contains__(OK_MSG):
+                        break
+                    file1.write(data)
+            end1 = time.time()
+            cubic.append(float(end1-start1))
+            print(f"First file has been received")
+
+            client.sendall(AUTH)
+            time.sleep(TIMEOUT)
+            print("Authentication has been sent.")
+
+            client.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, b"reno")
+            print("Changing CC algorithem to reno.")
+
+            start2 = time.time()
+            with open("received_part2.txt", "wb") as file2:
+                while True:
+                    data = client.recv(BUFFER_SIZE)
+                    if data.__contains__(OK_MSG):
+                        break
+                    file2.write(data)
+            end2 = time.time()
+            reno.append(float(end2-start2))
+            print(f"Second file has been received")
+
+            msg = client.recv(1024).decode()
+            if msg == "again":
+                continue
+            elif msg == "bye":
+                print("##############RESULTS##############")
+                print("File-number      Cubic       Reno")
+                for i in range(len(cubic)): 
+                    print(f"seq = {i} cubic = {cubic[i]} reno = {reno[i]}")
+                client.close()
+                break
+
+
+if __name__ == "__main__":
+    main()
+
--- Sender.py
+++ Sender.py
@@ -1,29 +1,105 @@
 import os
-import socket 
+import socket
+import io
+import sys
+import time
+
+HALF_SIZE = (os.path.getsize("file.txt") // 2)  # ...
+SERVER_ADDR = ("127.0.0.1", 8888)  # ...
+BUFFER_SIZE = 8192 # ...
+AUTH = f"{(8039) ^ (7214)}".encode()  # ...
+FIRST_HALF_FILE = "first_half.txt"
+SECOND_HALF_FILE = "second_half.txt"
+FILE = "file.txt"
+TIMEOUT = 0.001
+OK_MSG = b"Yuval&Ron"
+FAIL = 1
+SUCCESS = 0
+AGAIN = b"again"
+BYE = b"bye"
+
+
+def divide_file(file):
+    """
+    TODO describe about function...
+    :param file:
+    """
+    try:
+        first = file.read(HALF_SIZE)
+        file.seek(HALF_SIZE)
+        second = file.read()
+
+        with open(FIRST_HALF_FILE, "wb") as f1:
+           f1.write(first)
+    
+        with open(SECOND_HALF_FILE, "wb") as f2:
+            f2.write(second)
+    except Exception as e:
+        print(e)
+        sys.exit(FAIL)
+
 
 def main():
-    fp = open('file.txt', 'rb') # Open the file in read mode
-    file_size = os.path.getsize('file.txt') # tell the file size in bytes
-    half_size = file_size//2
-    first_half = fp.read(half_size)
-    fp.seek(half_size)
-    second_half = fp.read()
-    fp1 = open('first_half.txt', 'wb') # Write the first half to a new file
-    fp1.write(first_half)
-    fp2 = open('second_half.txt', 'wb') # Write the second half to a new file
-    fp2.write(second_half)
+    
+    try:
+        with open(FILE, "rb") as file:
+            divide_file(file)
 
-    sender_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a socket
-    receiver_addr = ('127.0.0.1', 8888)
-    sender_socket.connect(receiver_addr) # connecting to the receiver
-    sender_socket.sendall(first_half) # sending the first half of the file
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP) as client_sock:
+            client_sock.connect(SERVER_ADDR)
+            while True:
+                client_sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, b"cubic")
 
-    auth = sender_socket.recv(1024)
-    print('auth = ', auth.decode())
+                with open(FIRST_HALF_FILE, "rb") as file:
+                    while True:
+                        data = file.read(BUFFER_SIZE)
+                        if not data:
+                            break
+                        client_sock.sendall(data)
+                        time.sleep(TIMEOUT)
+                print("First half has been sent.")
 
+                client_sock.sendall(OK_MSG)
+                time.sleep(TIMEOUT)
+                
+                auth = client_sock.recv(BUFFER_SIZE)
+                if auth != AUTH:
+                    print("Authintication doesnt match.\nExiting...")
+                    sys.exit(FAIL)
+                print("Authentication match.")
 
+                client_sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, b"reno")
 
+                with open(SECOND_HALF_FILE, "rb") as file:
+                    while True:
+                        data = file.read(BUFFER_SIZE)
+                        if not data:
+                            break
+                        client_sock.sendall(data)
+                        time.sleep(TIMEOUT)
+                print("Second file has been sent.")
+
+                client_sock.sendall(OK_MSG)
+                time.sleep(TIMEOUT)
+
+                desicion = input("Send file again? (y/n)")
+                if desicion == "y" or desicion == "Y":
+                    client_sock.sendall(AGAIN)
+                    time.sleep(TIMEOUT)
+                    continue
+                elif desicion == "n" or desicion == "N":
+                    print("Exiting...")
+                    client_sock.sendall(BYE)
+                    time.sleep(TIMEOUT)
+                    client_sock.shutdown(socket.SHUT_RDWR)
+                    break
+    except Exception as e:
+        print(e)
+        sys.exit(FAIL)
 
 
 if __name__ == "__main__":
     main()
+    sys.exit(SUCCESS)
+
+
